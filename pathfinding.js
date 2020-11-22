435345e40a21136d8a8ac84ca8cc64b84e3c77c5
wanzenried <wanzenried@users.noreply.github.com>
2020-11-22 21:28:33 +0100

    started adding pathfinding algorithm

--- pathfinding.js
+++ pathfinding.js
@@ -5,6 +5,11 @@ class Pathfinder {
 
   navigate(x1,y1,x2,y2){
     console.log("going from " +x1+ ","+y1+" to " + x2+","+y2);
+    let start = createVector(x1,y1);
+    let goal = createVector(x2,y2);
+
+    let dir = p5.Vector.sub(goal,start);
+    console.log(dir);
   }
 
 }
